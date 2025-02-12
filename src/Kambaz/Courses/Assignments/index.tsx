dd2bb2793b66065bd89741a807514a5aa6c47ab7
Ashley Davis <ashley921davis@gmail.com>
2025-02-12 16:03:48 -0500

    Data driven assignments

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -3,8 +3,12 @@ import { BsGripVertical, BsCaretDownFill, BsSearch, BsPlusLg } from "react-icons
 import { FaRegListAlt } from "react-icons/fa";
 import AssignmentControlButtonGroup from "./AssignmentControlButtonGroup";
 import { IoEllipsisVertical } from "react-icons/io5";
+import { useParams } from "react-router";
+import * as db from "../../Database";
 
 export default function Assignments() {
+    const { cid } = useParams();
+    const assignments = db.assignments;
     return (
         <div>
             <div className="d-flex justify-content-between align-items-center mb-4">
@@ -46,53 +50,34 @@ export default function Assignments() {
                     </div>
 
                     <ListGroup className="rounded-0">
-                        {[
-                            {
-                                id: "A1",
-                                title: "ENV + HTML",
-                                date: "May 6",
-                                dueDate: "May 13",
-                                points: "100"
-                            },
-                            {
-                                id: "A2",
-                                title: "CSS + BOOTSTRAP",
-                                date: "May 13",
-                                dueDate: "May 20",
-                                points: "100"
-                            },
-                            {
-                                id: "A3",
-                                title: "JAVASCRIPT + REACT",
-                                date: "May 20",
-                                dueDate: "May 27",
-                                points: "100"
-                            }
-                        ].map((assignment) => (
-                            <ListGroup.Item
-                                key={assignment.id}
-                                className="border-1"
-                            >
-                                <div className="d-flex align-items-center py-2">
-                                    <BsGripVertical className="me-2 text-secondary" />
-                                    <FaRegListAlt className="me-2 text-success" />
-                                    <div className="flex-grow-1">
-                                        <a href="#/Kambaz/Courses/1234/Assignments/123"
-                                            className="wd-assignment-link fw-bold text-decoration-none text-dark" >
-                                            {assignment.id} - {assignment.title}
-                                        </a>
-                                        <br />
-                                        <small>
-                                            <span className="text-danger">Multiple Modules</span>
-                                            <span className="text-secondary"> | Not available until {assignment.date} at 12:00am |</span>
+                        {assignments
+                            .filter((assignment: any) => assignment.course === cid)
+
+                            .map((assignment: any) => (
+                                <ListGroup.Item
+                                    key={assignment._id}
+                                    className="border-1"
+                                >
+                                    <div className="d-flex align-items-center py-2">
+                                        <BsGripVertical className="me-2 text-secondary" />
+                                        <FaRegListAlt className="me-2 text-success" />
+                                        <div className="flex-grow-1">
+                                            <a href="#/Kambaz/Courses/1234/Assignments/123"
+                                                className="wd-assignment-link fw-bold text-decoration-none text-dark" >
+                                                {assignment._id} - {assignment.title}
+                                            </a>
                                             <br />
-                                            <span className="text-secondary">Due {assignment.dueDate} at 11:59pm | {assignment.points} pts</span>
-                                        </small>
+                                            <small>
+                                                <span className="text-danger">Multiple Modules</span>
+                                                <span className="text-secondary"> | Not available until {assignment.date} at 12:00am |</span>
+                                                <br />
+                                                <span className="text-secondary">Due {assignment.dueDate} at 11:59pm | {assignment.points} pts</span>
+                                            </small>
+                                        </div>
+                                        <AssignmentControlButtonGroup />
                                     </div>
-                                    <AssignmentControlButtonGroup />
-                                </div>
-                            </ListGroup.Item>
-                        ))}
+                                </ListGroup.Item>
+                            ))}
                     </ListGroup>
                 </ListGroup.Item>
             </ListGroup>
