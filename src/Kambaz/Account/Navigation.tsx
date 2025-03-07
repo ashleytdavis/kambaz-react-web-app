1d0e5354837d3d5c14174001846f34c63b04bfa9
Ashley Davis <ashley921davis@gmail.com>
2025-03-07 16:52:07 -0500

    Account Navigation

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 export default function AccountNavigation() {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
     return (
-        <div id="wd-account-navigation">
-            <Link to={`/Kambaz/Account/Signin`}  > Signin  </Link> <br />
-            <Link to={`/Kambaz/Account/Signup`}  > Signup  </Link> <br />
-            <Link to={`/Kambaz/Account/Profile`} > Profile </Link> <br />
+        <div id="wd-account-navigation" className="d-flex flex-column">
+            {links.map((link: any) => (
+                <Link to={`/Kambaz/Account/${link}`} key={link} > {link} </Link>
+            ))}
         </div>
     );
 }
