5ef16bb7689490b512d0ebeeb52d6da2a3ea620f
Ashley Davis <ashley921davis@gmail.com>
2025-04-08 17:32:48 -0400

    linter

--- src/Kambaz/Courses/People/Details.tsx
+++ src/Kambaz/Courses/People/Details.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
-import { useParams, useNavigate, Form } from "react-router";
+import { useParams, useNavigate } from "react-router";
 import { FaPencil } from "react-icons/fa6";
 import { FaCheck, FaUserCircle } from "react-icons/fa";
 import * as client from "../../Account/client";
